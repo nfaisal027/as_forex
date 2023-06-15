69fbf18421ada71d966318dc6b85e4c174aedc55
nfaisal027 <faisalnur969@gmail.com>
2023-06-15 12:19:59 +0700

    frx

---
+++ app/src/main/java/com/nurfaisal_202102340/forex/RootModel.java
@@ -0,0 +1,17 @@
+package com.nurfaisal_202102340.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+import kotlin.jvm.internal.SerializedIr;
+
+public class RootModel
+{
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public RootModel() {}
+
+    public RatesModel getRatesModel() { return ratesModel; }
+
+    public void setRatesModel (RatesModel ratesModel) {this.ratesModel = ratesModel; }
+}
